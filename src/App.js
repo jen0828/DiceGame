73cc1df902f6fcdf011126fed5b4490f85ac5bf8
Jenny Moon <jenny0moon@gmail.com>
2023-03-04 16:24:45 +0000

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import { useState } from 'react';
 import Die from './Die';
 
 export default function App() {
-  const [dice, setDice] = useState({ value: allNewDice(), isHeld: false });
+  const [dice, setDice] = useState(allNewDice());
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
     }
     return newDice;
   }
 
-  const diceElements = dice.value.map((die, index) => (
-    <Die value={die} key={index} />
+  const diceElements = dice.map((die, index) => (
+    <Die value={die.value} key={index} />
   ));
 
   function handleClick() {
-    setDice({ value: allNewDice(), isHeld: false });
+    setDice(allNewDice());
   }
 
   return (
