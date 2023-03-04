657b0fb0737e6590ab7681ad7c8165e5d127ea59
Jenny Moon <jenny0moon@gmail.com>
2023-03-04 18:54:23 +0000

    Add styling

--- src/App.js
+++ src/App.js
@@ -48,6 +48,11 @@ export default function App() {
 
   return (
     <main>
+      <h1 className="title">Roll the dice! 🎲</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={handleClick}>
         Roll
