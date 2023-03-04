397254a44bbe6675d64696eebc2f7ec17ed99157
Jenny Moon <jenny0moon@gmail.com>
2023-03-04 16:31:49 +0000

    Import nanoid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Die from './Die';
+import { nanoid } from 'nanoid';
 
 export default function App() {
   const [dice, setDice] = useState(allNewDice());
@@ -7,13 +8,17 @@ export default function App() {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return newDice;
   }
 
-  const diceElements = dice.map((die, index) => (
-    <Die value={die.value} key={index} />
+  const diceElements = dice.map((die) => (
+    <Die value={die.value} key={die.id} />
   ));
 
   function handleClick() {
