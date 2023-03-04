f605ece52f4e0623cacc320f1adc89c7a24961df
Jenny Moon <jenny0moon@gmail.com>
2023-03-04 16:46:59 +0000

    Add conditional styling for die

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
   }
 
   const diceElements = dice.map((die) => (
-    <Die value={die.value} key={die.id} />
+    <Die value={die.value} key={die.id} isHeld={die.isHeld} />
   ));
 
   function handleClick() {
